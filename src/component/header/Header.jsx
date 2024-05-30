0c6fe18ff861a5c77f40c4fb5ef181fae67db2d2
sadikisept <sadikisept26@gmail.com>
2024-05-30 18:42:14 +0200

    insert nav links

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -3,8 +3,16 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className='header'>
+      <ul className='navlink'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header
